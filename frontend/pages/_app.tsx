5a2cc8e7be156bea1f723475f935219c883490ba
coffmanjrp <coffmanjrp@gmail.com>
2021-09-04 18:34:53 -0700

    Set default seo

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,14 @@
+import { DefaultSeo } from 'next-seo';
+import SEO from 'next-seo.config';
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
